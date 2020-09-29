ff0921e7bf19c53982201d0ebb62c518e22d423f
LoganGerber <logangerber1014@yahoo.com>
2020-09-29 05:12:12 -0700

    Add formatting options for prefix and suffix in Bar

--- Bar.py
+++ Bar.py
@@ -1,13 +1,23 @@
 import abc
 import math
+import string
 
 import _BaseProgress as base
 
 
+class _EndStringFormatter(string.Formatter):
+    def get_value(self, key, args, kwargs):
+        if isinstance(key, int):
+            return super().get_value(key, args, kwargs)
+        return kwargs[key] if key in kwargs else ''
+
+
 class Bar(base.BaseProgress):
+    _FORMATTER = _EndStringFormatter()
+
     def __init__(self, max_value: int = 100, current_value: int = 0, increment_by: float = 1, prefix: str = '', suffix: str = '', bar_width: int = 32, fill: str = '#', empty_fill: str = ' ', bar_prefix: str = '|', bar_suffix: str = '|'):
-        self._prefix = prefix
-        self._suffix = suffix
+        self._prefix = prefix if prefix else ''
+        self._suffix = suffix if suffix else ''
         self._bar_width = bar_width
         self._fill = fill
         self._empty_fill = empty_fill
@@ -19,7 +29,11 @@ class Bar(base.BaseProgress):
     def __str__(self):
         filled = math.floor(self._bar_width * self.progress)
         empty = self._bar_width - filled
-        return self._prefix + ' ' + self._bar_prefix + (self._fill * filled) + (self._empty_fill * empty) + self._bar_suffix + ' ' + self._suffix
+        prefix = self._formatEnd(
+            self._prefix + ' ') if self._prefix != '' else ''
+        suffix = self._formatEnd(
+            ' ' + self._suffix) if self._suffix != '' else ''
+        return prefix + self._bar_prefix + (self._fill * filled) + (self._empty_fill * empty) + self._bar_suffix + suffix
 
     @property
     def prefix(self) -> str:
@@ -27,7 +41,7 @@ class Bar(base.BaseProgress):
 
     @prefix.setter
     def prefix(self, val: str) -> None:
-        self._prefix = val
+        self._prefix = val if val else ''
 
     @property
     def suffix(self) -> str:
@@ -35,7 +49,7 @@ class Bar(base.BaseProgress):
 
     @suffix.setter
     def suffix(self, val: str) -> None:
-        self._suffix = val
+        self._suffix = val if val else ''
 
     @property
     def bar_width(self) -> int:
@@ -76,3 +90,6 @@ class Bar(base.BaseProgress):
     @bar_suffix.setter
     def bar_suffix(self, val: str) -> None:
         self._bar_suffix = val
+
+    def _formatEnd(self, end: str) -> str:
+        return Bar._FORMATTER.format(end, percent=(str(self.progress * 100) + '%'), current_value=self.current_value, max_value=self.max_value, remaining=self.remaining)
